dec1b6b1c2956bff46d31b89b2e976fc5d2f194d
Rob Birch <rbirchtree@gmail.com>
2020-05-04 12:43:51 -0700

    added docker file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, send_from_directory
-from flask_mail import Mail
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -9,7 +8,6 @@ app = Flask(__name__)
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-mail = Mail(app)
 migrate = Migrate(app,db)
 
 from app import routes, models, errors
