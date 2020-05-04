173ad7d05749adeed8a5b150bc1d0e21833504e5
Rob Birch <rbirchtree@gmail.com>
2020-05-03 23:36:45 -0700

    added some css

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, send_from_directory
 from flask_bootstrap import Bootstrap
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
